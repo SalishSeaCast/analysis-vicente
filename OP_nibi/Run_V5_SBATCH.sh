89f69749f1be0c993e33dffaf5b1b4d7328141e4
valenzuela12 <vvalenzuela@eoas.ubc.ca>
2026-05-03 15:24:18 -0400

    V4 and V5 in process (+updates)

---
+++ OP_nibi/Run_V5_SBATCH.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#SBATCH --job-name=NOV-DEC-V5
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --mem-per-cpu=16000M
+#SBATCH --time=100:00:00
+#SBATCH --mail-user=vvalenzuela@eoas.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+# stdout and stderr file paths/names
+#SBATCH --output=/home/vicentev/scratch/vicentev/files_out_err/STDOUT
+#SBATCH --error=/home/vicentev/scratch/vicentev/files_out_err/STDERR
+#
+source /home/vicentev/miniforge3/etc/profile.d/conda.sh
+conda activate Parcels
+#
+WORKDIR=/home/vicentev/projects/def-allen/vicentev/analysis-vicente/OP_nibi
+DRIVER=Model_Driver_V2_final
+CONFIG_P1=$WORKDIR/config_files_V5/config_file_V5_P1.yaml
+CONFIG_P2=$WORKDIR/config_files_V5/config_file_V5_P2.yaml
+CONFIG_P3=$WORKDIR/config_files_V5/config_file_V5_P3.yaml
+CONFIG_P4=$WORKDIR/config_files_V5/config_file_V5_P4.yaml
+CONFIG_P5=$WORKDIR/config_files_V5/config_file_V5_P5.yaml
+CONFIG_P6=$WORKDIR/config_files_V5/config_file_V5_P6.yaml
+CONFIG_P7=$WORKDIR/config_files_V5/config_file_V5_P7.yaml
+CONFIG_P8=$WORKDIR/config_files_V5/config_file_V5_P8.yaml
+CONFIG_P9=$WORKDIR/config_files_V5/config_file_V5_P9.yaml
+CONFIG_P10=$WORKDIR/config_files_V5/config_file_V5_P10.yaml
+CONFIG_P11=$WORKDIR/config_files_V5/config_file_V5_P11.yaml
+CONFIG_P12=$WORKDIR/config_files_V5/config_file_V5_P12.yaml
+#
+out_txt_path=/home/vicentev/scratch/vicentev/out
+#
+cd $WORKDIR
+#
+#python -m "$DRIVER" "$CONFIG_P1" &> "$out_txt_path/output_V5_P1.txt" &
+#python -m "$DRIVER" "$CONFIG_P2" &> "$out_txt_path/output_V5_P2.txt" &
+#python -m "$DRIVER" "$CONFIG_P3" &> "$out_txt_path/output_V5_P3.txt" &
+#python -m "$DRIVER" "$CONFIG_P4" &> "$out_txt_path/output_V5_P4.txt" &
+#python -m "$DRIVER" "$CONFIG_P5" &> "$out_txt_path/output_V5_P5.txt" &
+#python -m "$DRIVER" "$CONFIG_P6" &> "$out_txt_path/output_V5_P6.txt" &
+#python -m "$DRIVER" "$CONFIG_P7" &> "$out_txt_path/output_V5_P7.txt" &
+#python -m "$DRIVER" "$CONFIG_P8" &> "$out_txt_path/output_V5_P8.txt" &
+#python -m "$DRIVER" "$CONFIG_P9" &> "$out_txt_path/output_V5_P9.txt" &
+#python -m "$DRIVER" "$CONFIG_P10" &> "$out_txt_path/output_V5_P10.txt" &
+python -m "$DRIVER" "$CONFIG_P11" &> "$out_txt_path/output_V5_P11.txt" &
+python -m "$DRIVER" "$CONFIG_P12" &> "$out_txt_path/output_V5_P12.txt" &
+#
+wait
+echo "Version 5 Done! :D"  
--- OP_nibi/concatenation.py
+++ OP_nibi/concatenation.py
@@ -1,22 +1,21 @@
 import xarray as xr
 from pathlib import Path
 
-BASE_DIR = Path('/home/vicentev/scratch/vicentev/Simulation_V2/')
+BASE_DIR = Path('/home/vicentev/scratch/vicentev/Simulation_V4/')
 
-files = [
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P1.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P2.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P3.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P4.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P5.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P6.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P7.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P8.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P9.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P10.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P11.nc',
-    'PBDEs_0112007_run_365_days_Tau_0_0025_Ads_0_05_MC_0_2_Vel_N_P12.nc',
-    ]
+files = ['PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P1.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P2.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P3.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P4.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P5.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P6.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P7.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P8.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P9.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P10.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P11.nc',
+         'PBDEs_0112007_run_365_days_Tau_0_001_Ads_0_01_MC_0_2_Vel_Hx1_2_P12.nc'
+]
 
 # Full paths
 file_paths = [BASE_DIR / f for f in files]
@@ -25,11 +24,11 @@ file_paths = [BASE_DIR / f for f in files]
 ds = xr.open_mfdataset(
     file_paths,
     combine='nested',
-    concat_dim='obs'
+    concat_dim='trajectory'
 )
 
 # Save result
-output_path = BASE_DIR / 'Simulation_V2_year_1.nc'
+output_path = BASE_DIR / 'Simulation_V4_year_1.nc'
 ds.to_netcdf(output_path)
 
 print(f"Saved to {output_path}")
